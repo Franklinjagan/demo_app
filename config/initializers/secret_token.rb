cd017467f0f1bcd02e77b4a2c6862a438a18b831
arrivu <arrivu@arrivu-Satellite-C675.(none)>
2013-06-18 10:50:17 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '043b3be30ac60e653ef0cd1afdc7c61109a81cce2db980c7dd678de4d6d37162f92df47acfe36bc1a03c33b02a3f188ca86a0a5f8cf7d4142699a5ed0ab34c85'
